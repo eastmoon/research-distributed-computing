d7a4b5386573246378033d4a93ea1368abf8402e
Jacky Eastmoon <jacky_eastmoon@hotmail.com>
2022-10-31 10:11:45 +0800

    feat: initial virtual machine script

---
+++ src/install-virtual-environment.sh
@@ -0,0 +1,35 @@
+# Install Docker
+# Ref : https://docs.docker.com/engine/install/ubuntu/
+sudo apt-get update -y && \
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update -y && \
+sudo apt-get install -y \
+    docker-ce \
+    docker-ce-cli \
+    docker-compose \
+    containerd.io
+
+# Install virtualbox
+wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] http://download.virtualbox.org/virtualbox/debian $(lsb_release -cs) contrib"
+sudo apt-get update -y && \
+sudo apt-get install -y virtualbox-6.1
+
+# Install vagrant
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt-get update -y && \
+sudo apt-get install -y vagrant
